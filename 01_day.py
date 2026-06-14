67b2613c8cb5d1176cb7e43df358f7431f120857
vishnunukala97 <vishnuvarusa@gmail.com>
2026-06-13 17:54:49 -0700

    files upload

---
+++ 01_day.py
@@ -0,0 +1,48 @@
+"""
+ Challenge: File Sorter by Type
+
+Goal:
+- Scan the current folder (or a user-provided folder)
+- Move files into subfolders based on their type:
+    - .pdf → PDFs/
+    - .jpg, .jpeg, .png → Images/
+    - .txt → TextFiles/
+    - Others → Others/
+- Create folders if they don't exist
+- Ignore folders during the move
+
+Teaches: File system operations, automation, file handling with `os` and `shutil`
+"""
+
+import os
+import shutil
+
+EXTENSION_MAP = {
+    "PDFs":[".pdf"],
+    "Images":[".png",".jpg",".jpeg"],
+    "TextFiles": [".txt"]
+}
+
+def get_destination_folder(filename):
+    ext = os.path.splitext(filename)[1].lower()
+    for folder, extensions in EXTENSION_MAP:
+        if ext in extensions:
+            return folder
+        
+    return "Others"
+
+def sort_files(folder_path):
+    for file in os.listdir(folder_path):
+        full_path = os.path.join(folder_path,file)
+
+        if os.path.isfile(full_path):
+            dest_folder = get_destination_folder(file)
+            dest_path = os.path.join(folder_path,dest_folder)
+
+            os.makedirs(dest_path,exist_ok=True)
+
+            #Move the file
+            shutil.move(full_path,os.path.join(dest_path,file))
+            print(f"Moved : {file} -> {dest_folder}/")
+
+
