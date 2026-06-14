80ac8a6536a976e059a63e5eed4539c2e556a9cd
vishnunukala97 <vishnuvarusa@gmail.com>
2026-06-13 18:12:20 -0700

    day 1 complete

--- 01_day.py
+++ 01_day.py
@@ -25,7 +25,7 @@ EXTENSION_MAP = {
 
 def get_destination_folder(filename):
     ext = os.path.splitext(filename)[1].lower()
-    for folder, extensions in EXTENSION_MAP:
+    for folder, extensions in EXTENSION_MAP.items():
         if ext in extensions:
             return folder
         
@@ -34,6 +34,11 @@ def get_destination_folder(filename):
 def sort_files(folder_path):
     for file in os.listdir(folder_path):
         full_path = os.path.join(folder_path,file)
+
+        if file == "01_day.py":
+            continue
+
+        #write a check if file name is "01_day.py" then ignore this file
 
         if os.path.isfile(full_path):
             dest_folder = get_destination_folder(file)
@@ -44,5 +49,17 @@ def sort_files(folder_path):
             #Move the file
             shutil.move(full_path,os.path.join(dest_path,file))
             print(f"Moved : {file} -> {dest_folder}/")
+
+if __name__ == "__main__":
+    folder = input("Enter the folder path or leave blank: ").strip()
+    folder = folder or os.getcwd()
+
+    if not os.path.isdir(folder):
+        print("Invalid directory")
+    else:
+        sort_files(folder)
+        print("✅ Sorting completed ✅")
+
+
 
 
